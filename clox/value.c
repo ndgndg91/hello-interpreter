2394ab68132c2bf162bdd910a65690670a2b9a7b
ndgndg91 <ndgndg91@gamil.com>
2024-04-18 00:27:07 +0900

    value type

--- clox/value.c
+++ clox/value.c
@@ -3,6 +3,16 @@
 #include "memory.h"
 #include "value.h"
 
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL: return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default:return false; // 실행되지 않는 코드
+    }
+}
+
 void initValueArray(ValueArray* array) {
     array->values = NULL;
     array->capacity = 0;
@@ -24,5 +34,17 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+
+    }
+
 }
