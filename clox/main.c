a75a4c25460736517a13cf1bd4c543768316cf8a
ndgndg91 <ndgndg91@gamil.com>
2024-04-21 17:03:34 +0900

    tidy import main

--- clox/main.c
+++ clox/main.c
@@ -2,11 +2,8 @@
 //> Scanning on Demand main-includes
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "common.h"
-#include "chunk.h"
-#include "debug.h"
 #include "vm.h"
 
 static void repl() {
